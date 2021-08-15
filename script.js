440346c3518448054442c6a6b0218d0f91b50366
Juan Moreno <morenostudent1@gmail.com>
2021-08-15 02:43:46 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 const menu = document.getElementById('navbar-menu')
 const toggleMenu = document.getElementById('toggle-menu')
-const closeMenu = document.getElementById('close')
+const closeButton = document.getElementById('close')
 
-toggleMenu.addEventListener('click', toggleMenu)
-closeMenu.addEventListener('click', closeMenu)
+toggleMenu.addEventListener('click', openMenu)
+closeButton.addEventListener('click', closeMenu)
 
 window.addEventListener('click', outsideClick)
-outsideClick = (e) => {
+
+function outsideClick(e) {
     if(e.target === menu & e.target === document.body){
         closeMenu()
     }
 }
 
-function toggleMenu(){
+function openMenu(){
     menu.style.display = 'block'
 }
 
