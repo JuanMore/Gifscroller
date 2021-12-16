5eecfbb4d6d88ec1f27d1f8136e403db47436040
Juan <morenostudent1@gmail.com>
2021-12-16 00:51:43 -0800

    login modal

--- script.js
+++ script.js
@@ -39,33 +39,6 @@ function closeLogIn(){
     modal.style.display = 'none'
 }
 
-// const loadGif = document.getElementById('gif-load')
-// const gifItem = [];
-// // getData()
 
-// async function getData(){
-//     try {
-//     const res = await fetch('https://api.thecatapi.com/images//search?mime_types=gif', { params: { limit:1, size:"full" , mime_types: this.image_type } } , {
-//         method: 'GET',
-//         headers: {
-//             'X-API-KEY': '96653eb4-01f5-4a20-bb73-bf422ce50d1e'
-//         },
-//         body: JSON.stringify(getData)
-//     })
-//     .then(res => console.log(res))
-//     // result.forEach(res => {
-//     //     const gif = document.createElement('img')
-//     //     gifItem.push(gif)
-
-//     //     gif.innerHTML = ` 
-//     //     `
-//     //     console.log(res)
-
-//     // });
-// } 
-// catch(err){
-//     console.log(err)
-// }
-// }
 
 
